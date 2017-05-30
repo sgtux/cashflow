a40459fb8e90bc480408ed2920606f9d12746fa8
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2017-05-29 22:16:43 -0300

    Refactory

--- MyMvc/Startup.cs
+++ MyMvc/Startup.cs
@@ -10,9 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using MyMvc.Data;
-using MyMvc.Models;
-using MyMvc.Services;
 
 namespace MyMvc
 {
@@ -33,28 +30,8 @@ namespace MyMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            var dbUrl = Configuration?["DATABASE_URL"];
-            if (string.IsNullOrEmpty(dbUrl))
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                        options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
-            }
-            else
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                        options.UseNpgsql(dbUrl.ToConnectionString()));
-            }
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
 
             services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,7 +43,6 @@ namespace MyMvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
                 app.UseBrowserLink();
             }
             else
@@ -75,8 +51,6 @@ namespace MyMvc
             }
 
             app.UseStaticFiles();
-
-            app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
 
