6e09ab02b372ffb53b7e9c0916613fa705533f8e
Gabriel <gabrielsantoscimol@gmail.com>
2021-11-22 14:42:02 -0300

    Task #193 - Modelagem do banco de dados (#48)

---
+++ Migrations/DDL/202111171900_AddFuelExpensesTable.cs
@@ -0,0 +1,27 @@
+using FluentMigrator;
+
+namespace Cashflow.Migrations.DDL
+{
+    [Migration(202111171900)]
+    public class AddFuelExpensesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("FuelExpenses")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Miliage").AsInt32().NotNullable()
+                .WithColumn("ValueSupplied").AsDecimal(10, 2).NotNullable()
+                .WithColumn("PricePerLiter").AsDecimal(10, 2).NotNullable()
+                .WithColumn("VehicleId").AsInt32().NotNullable();
+
+            Create.ForeignKey()
+                .FromTable("FuelExpenses").ForeignColumn("VehicleId")
+                .ToTable("Vehicle").PrimaryColumn("Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("FuelExpenses");
+        }
+    }
+}
