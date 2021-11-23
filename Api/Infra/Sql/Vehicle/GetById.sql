11f8f7e38580ad4ab779cea5c2c620719a5d59ce
Gabriel Santos <sgtux@protonmail.com>
2021-11-23 17:03:39 -0300

    Task #208 - Ajustar métodos de obter/listar veículo, para carregar os gastos

--- Api/Infra/Sql/Vehicle/GetById.sql
+++ Api/Infra/Sql/Vehicle/GetById.sql
@@ -1,6 +1,12 @@
 SELECT 
-    "Id",
-    "Description",
-    "UserId"
-FROM "Vehicle"
-WHERE "Id" = @Id
+    v."Id",
+    v."Description",
+    v."UserId",
+    f."Id",
+    f."Miliage",
+    f."ValueSupplied",
+    f."PricePerLiter",
+    f."VehicleId"
+FROM "Vehicle" v
+LEFT JOIN "FuelExpenses" f ON v."Id" = f."VehicleId"
+WHERE v."Id" = @Id
