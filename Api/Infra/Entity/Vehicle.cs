2f799ba4f6f2261bc29e1fe950a04924b87a0086
Gabriel <gabrielsantoscimol@gmail.com>
2021-11-23 17:09:20 -0300

    Task #208 - Ajustar métodos de obter/listar veículo, para carregar os gastos (#50)

--- Api/Infra/Entity/Vehicle.cs
+++ Api/Infra/Entity/Vehicle.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Cashflow.Api.Infra.Entity
 {
     public class Vehicle
@@ -7,5 +9,7 @@ namespace Cashflow.Api.Infra.Entity
         public string Description { get; set; }
 
         public int UserId { get; set; }
+
+        public List<FuelExpenses> FuelExpenses { get; set; }
     }
 }
--- Api/Infra/Sql/Vehicle/GetByUser.sql
+++ Api/Infra/Sql/Vehicle/GetByUser.sql
@@ -1,7 +1,13 @@
 SELECT 
-    "Id",
-    "Description",
-    "UserId"
-FROM "Vehicle"
+    v."Id",
+    v."Description",
+    v."UserId",
+    f."Id",
+    f."Miliage",
+    f."ValueSupplied",
+    f."PricePerLiter",
+    f."VehicleId"
+FROM "Vehicle" v
+LEFT JOIN "FuelExpenses" f ON v."Id" = f."VehicleId"
 WHERE "UserId" = @UserId
-ORDER BY "Id"
+ORDER BY v."Id", f."Id"
